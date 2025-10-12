b998b3aeb43e6827f8c5f4a6c24999cbd6a38bd4
AcheronX. <richardhuynh53@gmail.com>
2025-10-12 14:00:23 -0700

    Update step 140

--- Building a Role Playing Game/Step 140/script.js
+++ Building a Role Playing Game/Step 140/script.js
@@ -193,12 +193,17 @@ function attack() {
 
   health -= monsters[fighting].level;
   monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
-
+  
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
     lose();
   } else if (monsterHealth <= 0) {
+    defeatMonster();
+    if (fighting === 2) {
+      winGame();
+    }
+  } else {
     defeatMonster();
   }
 }
