8182b5ae5b579acab35b6d68830c53d3276d69d3
AcheronX. <richardhuynh53@gmail.com>
2025-10-25 12:39:51 -0700

    Finished Step 40

---
+++ Part 2 Building a Decimal to Binary Converter/Step 40/script.js
@@ -0,0 +1,36 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const decimalToBinary = (input) => {
+  let binary = "";
+
+  while (input > 0) {
+    binary = input % 2;
+    input = Math.floor(input / 2);
+  }
+
+  result.innerText = binary;
+};
+
+const checkUserInput = () => {
+  if (
+    !numberInput.value ||
+    isNaN(parseInt(numberInput.value)) ||
+    parseInt(numberInput.value) < 0
+  ) {
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
